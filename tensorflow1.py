336bfc7a5245a4978e7181e6ceb2275b4b4f1d53
Levi Brackman <levibrackman@Levis-MacBook-Pro.local>
2019-04-24 01:22:03 -0600

    First commit

---
+++ tensorflow1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 00:12:21 2019
+
+@author: levibrackman
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
